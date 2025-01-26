c88686b30b1f152b3cd4f49e0b62fc5de1916aac
jassi2003 <145800527+jassi2003@users.noreply.github.com>
2025-01-26 22:38:52 +0530

    changes

--- backend/controllers/user/user.signup.js
+++ backend/controllers/user/user.signup.js
@@ -1,7 +1,7 @@
 // const express=require('express');
 // const userModel= require('../models/userModel')
 const userModel = require("../../models/userModel"); // Updated path
-const bcrypt=require('bcrypt');
+const bcrypt=require('bcryptjs');
 
 
 
